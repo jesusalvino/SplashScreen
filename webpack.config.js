d241443565c55b53b70b9b92e19837efa6660b6e
Aabishkar KC <aabishkar@gmail.com>
2022-10-05 12:00:03 -0400

    Specify webpack mode production in build script (#3)

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,46 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = (env) => {
-    return {
-        mode: 'development',
-        entry: {
-            app: './src/index.js',
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      app: './src/index.js',
+    },
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'index.bundle.js',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
         },
-        output: {
-            path: path.join(__dirname, '/dist/build'),
-            filename: 'index.bundle.js',
-            clean: true,
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader'
-                    }
-                },
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader']
-                }
-            ]
-        },
-        optimization: {
-            minimizer: [
-                new TerserPlugin({
-                    parallel: true,
-                    terserOptions: {
-                    },
-                }),
-            ]
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './public/index.html'
-            })
-        ]
-    };
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+          terserOptions: {
+          },
+        }),
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html'
+      })
+    ]
+  };
 };
